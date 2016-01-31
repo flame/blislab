7d8780d10de2b7830279dbdaf56355cdaf15292d
Jianyu Huang <jianyu@cs.utexas.edu>
2016-01-30 19:59:27 -0600

    Update compiling and executing scripts

--- src/step1/test/run_blis_dgemm.sh
+++ src/step1/test/run_blis_dgemm.sh
@@ -1,35 +1,29 @@
 #!/bin/bash
 #export DYLD_LIBRARY_PATH=/opt/intel/lib:/opt/intel/mkl/lib
 
-#export KMP_AFFINITY=compact
-#export OMP_NUM_THREADS=1
-#export BLISGEMM_IC_NT=1
-##Single Thread
-##m=1025
-##n=1025
-#kmax=1100
-#kstep=15
-#
-#echo "run_step1=["
-#for (( k=4; k<kmax; k+=kstep ))
-#do
-#    ./test_blis_dgemm.x     $k $k $k
-#done
-#echo "];"
+#Single Thread
+export KMP_AFFINITY=compact
+export OMP_NUM_THREADS=1
+export BLISGEMM_IC_NT=1
+kmax=1100
+kstep=15
+echo "run_step1_st=["
+for (( k=4; k<kmax; k+=kstep ))
+do
+    ./test_blis_dgemm.x     $k $k $k
+done
+echo "];"
 
 
+#Multi Thread
 export KMP_AFFINITY=compact
 export OMP_NUM_THREADS=10
 export BLISGEMM_IC_NT=10
 #Single Thread
-#m=1025
-#n=1025
 kmax=5000
 kstep=31
-
-echo "run_step1=["
+echo "run_step1_mt=["
 for (( k=4; k<kmax; k+=kstep ))
-#for (( k=4840; k<kmax; k+=kstep ))
 do
     ./test_blis_dgemm.x     $k $k $k
 done
