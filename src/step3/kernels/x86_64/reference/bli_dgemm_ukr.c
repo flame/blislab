38336c7d024b30806a9ef992dd212f62780b6c4c
Jianyu Huang <jianyu@cs.utexas.edu>
2016-02-04 16:35:50 -0600

    Simplify the reference C implementation of micro-kernels

--- src/step3/kernels/x86_64/reference/bli_dgemm_ukr.c
+++ src/step3/kernels/x86_64/reference/bli_dgemm_ukr.c
@@ -15,88 +15,25 @@ void bli_dgemm_ukr_ref( dim_t k,
                         aux_t* data )
 {
 
-    const inc_t cs_c = ldc;
-    const inc_t rs_c = 1;
-    double alpha_val = 1.0, beta_val = 1.0;
-    double *alpha, *beta;
-
-    alpha = &alpha_val;
-    beta  = &beta_val;
-
     const dim_t m = 8;
     const dim_t n = 4;
+
     const inc_t cs_a = 8;
     const inc_t rs_b = 4;
-    const inc_t rs_ab = 1;
-    const inc_t cs_ab = 8;
+
     dim_t l, j, i;
-    double ab[ 8 * 4 ];
-    double ai;
-    double bj;
-
-    //printf( "a: " );
-    //for ( l = 0; l < k; ++l ) {
-    //    for ( i = 0; i < m; ++i ) {
-    //        printf( "%lf ", a[ i*1 + l*cs_a ] );
-    //    }
-    //    printf( "\n" );
-    //}
-    //printf( "\n" );
-
-    for ( i = 0; i < m * n; ++i )
-    {
-        ((*(ab + i))) = (0.0);
-    }
 
     for ( l = 0; l < k; ++l )
     {                 
-        //double* restrict abij = ab;
-        double* abij = ab;
         for ( j = 0; j < n; ++j )
         { 
-            bj = *(b + j);
             for ( i = 0; i < m; ++i )
             { 
-                ai = *(a + i);
-                {
-                    (( *abij )) += (( ai )) * (( bj ));
-                };
-                abij += rs_ab;
+                c[i+j*ldc] = c[i+j*ldc] + a[i]*b[j];
             }
         }
         a += cs_a;
         b += rs_b;
-    }
-    for ( i = 0; i < m * n; ++i )
-    {
-        ( *(ab + i) ) = ( *alpha ) * ( *(ab + i) );
-    }
-
-    if (  (*beta) == 0.0  )
-    {
-        dim_t i, j;
-        for ( j = 0; j < n; ++j )
-            for ( i = 0;i < m; ++i )
-            {
-                (( *(c + i*rs_c + j*cs_c) )) = (( *(ab + i*rs_ab + j*cs_ab) ));
-            };
-    } else {
-        dim_t i, j;
-        if (  ((*beta)) == (0.0)  )
-        {
-            dim_t i, j;
-            for ( j = 0;j < n; ++j )
-                for ( i = 0;i < m; ++i )
-                {
-                    (( *(c + i*rs_c + j*cs_c) )) = (( *(ab + i*rs_ab + j*cs_ab) ));
-                };
-        } else {
-            for ( j = 0; j < n; ++j )
-                for ( i = 0; i < m; ++i )
-                {
-                    (( *(c + i*rs_c + j*cs_c) )) = (( *(ab + i*rs_ab + j*cs_ab) )) + (( *(beta) )) * (( *(c + i*rs_c + j*cs_c) ));
-                };
-        } 
     }
 
 }
