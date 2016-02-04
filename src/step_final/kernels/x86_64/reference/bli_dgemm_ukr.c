2cb3ee31e6316d0e00ff83b51056e81489a26068
Jianyu Huang <jianyu@cs.utexas.edu>
2016-02-04 17:23:47 -0600

    Simplify the reference C micro-kernel

--- src/step_final/kernels/x86_64/reference/bli_dgemm_ukr.c
+++ src/step_final/kernels/x86_64/reference/bli_dgemm_ukr.c
@@ -1,8 +1,5 @@
-
 #include <blis_config.h>
 #include <blis_dgemm.h>
-
-//#define unsigned long long inc_t
 
 //micro-panel a is stored in column major, lda=DGEMM_MR=8
 #define A(i,j) a[ (j)*DGEMM_MR + (i) ]
@@ -30,7 +27,6 @@ void bli_dgemm_ukr_ref( dim_t k,
         { 
             for ( i = 0; i < m; ++i )
             { 
-                //c[i+j*ldc] = c[i+j*ldc] + a[i+l*8]*b[j+l*4];
                 C(i,j) += A(i,l)*B(l,j);
             }
         }
