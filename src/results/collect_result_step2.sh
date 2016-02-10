63043f55ded066d770f75e45c5a8fa21e851541f
Jianyu Huang <jianyu@cs.utexas.edu>
2016-02-10 15:29:58 -0600

    Add collect_result scripts

---
+++ src/results/collect_result_step2.sh
@@ -0,0 +1,5 @@
+cd ../step2/test
+./run_bl_dgemm.sh | tee step2_result.m
+mv step2_result.m ../../results/.
+cd ../../results/.
+
---
+++ src/results/collect_result_step3.sh
@@ -0,0 +1,5 @@
+cd ../step3/test
+./run_bl_dgemm.sh | tee step3_result.m
+mv step3_result.m ../../results/.
+cd ../../results/.
+
