eda86ce27622d760282f8bf62f827866782fba36
Jianyu Huang <jianyu@cs.utexas.edu>
2016-02-13 17:33:16 -0600

    Update performance script

--- src/results/bl_dgemm_plot.m
+++ src/results/bl_dgemm_plot.m
@@ -2,6 +2,8 @@ step1_result;
 step2_result;
 step3_result;
 
+PEAK_PERF=28.32;
+
 % ---------------------------------------------------------
 % Plotting
 % ---------------------------------------------------------
@@ -16,14 +18,14 @@ set( gcf, 'Position', [0 0 600 400]);
 
 hold;
 
-%plot( run_step1_st( :, 1 ), run_step1_st( :,4), '.-', 'LineWidth', 2, 'Color',  [0 0.2 1.0] );
-%plot( run_step1_st( :, 1 ), run_step1_st( :, 5), '.-', 'LineWidth', 2, 'Color', [1 0 0.2] );
+plot( run_step1_st( :, 1 ), run_step1_st( :,4), '.-', 'LineWidth', 2, 'Color',  [0 0.2 1.0] );
+plot( run_step1_st( :, 1 ), run_step1_st( :, 5), '.-', 'LineWidth', 2, 'Color', [1 0 0.2] );
+
+%plot( run_step2_st( :, 1 ), run_step2_st( :,4), '.-', 'LineWidth', 2, 'Color',  [0 0.2 1.0] );
+%plot( run_step2_st( :, 1 ), run_step2_st( :, 5), '.-', 'LineWidth', 2, 'Color', [1 0 0.2] );
 
 %plot( run_step3_st( :, 1 ), run_step3_st( :,4), '.-', 'LineWidth', 2, 'Color',  [0 0.2 1.0] );
 %plot( run_step3_st( :, 1 ), run_step3_st( :, 5), '.-', 'LineWidth', 2, 'Color', [1 0 0.2] );
-
-plot( run_step10_st( :, 1 ), run_step10_st( :,4), '.-', 'LineWidth', 2, 'Color',  [0 0.2 1.0] );
-plot( run_step10_st( :, 1 ), run_step10_st( :, 5), '.-', 'LineWidth', 2, 'Color', [1 0 0.2] );
 
 
 xlabel( 'm=k=n' );
@@ -32,18 +34,18 @@ title( 'DGEMM(m=k=n)' );
 
 grid on;
 axis square;
-axis( [ 0 1030 0 28.32 ] );
+axis( [ 0 1024 0 PEAK_PERF ] );
 %axis( [ 0 5000 0 248 ] );
 
 ax = gca;
-ax.YTick = [  0, 5, 10, 15, 22, 28.32 ];
+ax.YTick = [  0, 5, 10, 15, 20, 25, PEAK_PERF ];
 %ax.YTick = [  0, 50, 100, 150, 200, 248];
 
 ax.XTick = [ 0, 200, 400, 600, 800, 1000];
 %ax.XTick = [ 0, 1000, 2000, 3000, 4000, 5000 ];
 set( gca,'FontSize',14 );
 
-legend( 'dgemm\_step10\_st', ...
+legend( 'dgemm\_step1\_st', ...
         'dgemm\_mkl\_st', ...
         'Location','SouthEast');
 
